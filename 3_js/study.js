0ac8e9b83a0975d6637f85da62079db5565bed0c
Andrew <rtpdfv@yahoo.com>
2022-02-08 18:30:38 +0200

    js strings.

--- 3_js/study.js
+++ 3_js/study.js
@@ -1,3 +1,19 @@
-console.log("abcIHiuIUhiuHihUi".length);
-console.log("abcIHiuIUhiuHihUi".toLowerCase());
-console.log("abcIHiuIUhiuHihUi".toUpperCase());
+/**
+* @param {string} name
+*/
+
+let abc = "abcIHiuIUhiuHihUi";
+console.log(abc.length);
+console.log(abc.toLowerCase());
+console.log(abc.toUpperCase());
+console.log(abc[abc.length - 2]); // 2nd char from end
+
+
+let hello_world = 'Hello world!';
+let hello = hello_world.substring(0, 5);
+let world = hello_world.substring(6);
+console.log(hello);
+console.log(world);
+
+hello_world = hello + ' ' + world;
+console.log(hello_world);
