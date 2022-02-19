5119e08320c5943a5fef8330f0218acfeaf9e4af
Andrew <rtpdfv@yahoo.com>
2022-02-19 14:09:26 +0200

    .

--- 4_html_scss_typescript/simple_server.js
+++ 4_html_scss_typescript/simple_server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.listen(3300, () => {
+app.listen(3200, () => {
   console.log("Application started and Listening on port 3000");
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/hello.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/style.css", (req, res) => {
-  res.sendFile(__dirname + "/style.css");
+app.get("/styles/style.css", (req, res) => {
+  res.sendFile(__dirname + "./styles/style.css");
 });
 
-app.get("/123.png", (req, res) => {
-  res.sendFile(__dirname + "/123.png");
+app.get("/scripts/main.js", (req, res) => {
+  res.sendFile(__dirname + "./scripts/main.js");
+});
+
+app.get("/images/favicon.png", (req, res) => {
+  res.sendFile(__dirname + "./images/favicon.png");
+});
+app.get("/images/firefox.png", (req, res) => {
+  res.sendFile(__dirname + "./images/firefox.png");
+});
+app.get("/images/firefox2.png", (req, res) => {
+  res.sendFile(__dirname + "./images/firefox2.png");
 });
