791d86ffb8fe55f15a965c0ee78e722f8094b171
Andrew <rtpdfv@yahoo.com>
2022-03-06 12:36:33 +0200

    JS  Silly story generator.

--- 3_js/2_silly_story_generator/main.js
+++ 3_js/2_silly_story_generator/main.js
@@ -7,7 +7,6 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
@@ -16,18 +15,21 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result() {
-
-  if(customName.value !== '') {
-    const name = customName.value;
-
-  }
-
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
+    let newStory = storyText;
+    newStory = newStory.replace(':insertx:', randomValueFromArray(insertX));
+    newStory = newStory.replace(':insertx:', randomValueFromArray(insertX));
+    newStory = newStory.replace(':inserty:', randomValueFromArray(insertY));
+    newStory = newStory.replace(':insertz:', randomValueFromArray(insertZ));
+    if(customName.value !== '') {
+        const name = customName.value;
+        newStory = newStory.replace('Bob', name);
+    }
+    if(document.getElementById("uk").checked) {
+        const weight = Math.round(300 * 0.071428571428571) + ' stone';
+        const temperature = Math.round((94-32)*(5/9)) + ' centigrade';
+        newStory = newStory.replace('300 pounds', weight);
+        newStory = newStory.replace('94 fahrenheit', temperature);
+    }
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
 }
