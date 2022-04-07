22e94b994225667150163cdebc9d4364877c985c
Andrew <rtpdfv@yahoo.com>
2022-04-07 17:33:50 +0200

    .

--- 4_html_scss_typescript/scripts/main.js
+++ 4_html_scss_typescript/scripts/main.js
@@ -1,2 +1,37 @@
-let a:string = "123";
+var a = "str";
 console.log(a);
+a = "123";
+var body = document.querySelector("body");
+body.style.backgroundColor = "white";
+var neuronNetwork = /** @class */ (function () {
+    function neuronNetwork(neurons, synapses) {
+        this._neurons = [];
+        this._synapses = [];
+        this._neurons = neurons;
+        this._synapses = synapses;
+    }
+    Object.defineProperty(neuronNetwork.prototype, "neurons", {
+        get: function () {
+            return this._neurons;
+        },
+        set: function (value) {
+            this._neurons = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(neuronNetwork.prototype, "synapses", {
+        get: function () {
+            return this._synapses;
+        },
+        set: function (value) {
+            this._synapses = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return neuronNetwork;
+}());
+var a = new neuronNetwork([1, 2, 3], [4, 5, 6]);
+console.log(a.neurons);
+console.log(a.synapses);
--- 4_html_scss_typescript/scripts/main.ts
+++ 4_html_scss_typescript/scripts/main.ts
@@ -1,2 +1,7 @@
 let a: string = "str";
 console.log(a);
+
+a = "123";
+
+let body = document.querySelector("body");
+body.style.backgroundColor = "white";
